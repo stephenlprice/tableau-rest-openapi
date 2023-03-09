532dac645900540bff8b54e0d75c4ad30f8d3b9c
the-architect <stellopricint@gmail.com>
2023-03-09 11:09:27 -0600

    uploading new spec

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjg1MDk4/spectral.js?branch_slug=main&token=0c20ef97-16d1-4f36-b3af-4667f1b3d350";
+export default { extends: ruleset };
